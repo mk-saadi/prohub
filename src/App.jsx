84ffa256784ec613fed427282481fd6af4dac688
mk-saadi <mksaadi820@gmail.com>
2023-03-30 22:49:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<h1 className="display-5">hello world</h1>
+		</div>
+	);
+}
+
+export default App;
